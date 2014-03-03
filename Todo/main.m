4019c0c1bd92da5288abb6628bb216b5379bb985
Matt Nguyen <mnguyen916@gmail.com>
2014-03-03 15:12:18 +0700

    Initial Commit

---
+++ Todo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Todo
+//
+//  Created by Matthew Nguyen on 3/3/14.
+//  Copyright (c) 2014 Matthew Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
